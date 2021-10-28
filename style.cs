b72fa4ef9c080270cc0055c9e32347f02fc50ad0
rithik0105 <82500666+rithik0105@users.noreply.github.com>
2021-10-28 11:27:47 +0530

    Add files via upload

---
+++ style.cs
@@ -0,0 +1,18 @@
+body {
+  color:blue;
+  background: pink;
+
+}     
+.heading{
+    width: 80%;
+    font-size:22px;
+}
+#camera{
+    border: 2px deeppink ;
+    width:350px;
+    height:300px;
+}
+.name_of_object,#result_object_name,#result_object_accuracy{
+    color:deeppink;
+    
+}
